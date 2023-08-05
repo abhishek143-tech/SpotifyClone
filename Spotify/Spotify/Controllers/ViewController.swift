d632a1c37fd440b47d26e8ee946ff875c2b3cb2b
Abhishek Dhok <abhishekdhok96@gmail.com>
2023-08-05 22:00:16 +0530

    Initial Commit

---
+++ Spotify/Spotify/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Abhishek Dilip Dhok on 05/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
